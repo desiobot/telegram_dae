d55140513792e0118ad952dad8204aed607fea2f
Paolo Fisco <paolo.fisco@gmail.com>
2016-06-13 16:57:38 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,4 +1,26 @@
 <?php
+
+function sendImage($idChat, $linkImage) {
+	$bot_url    = "https://api.telegram.org/bot<bot_id>/";
+	$url        = $bot_url . "sendPhoto?chat_id=" . $chat_id ;
+	
+	$post_fields = array('chat_id'   => $chat_id,
+	    'photo'     => new CURLFile(realpath("/path/to/image.png"))
+	);
+	
+	$ch = curl_init(); 
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+	    "Content-Type:multipart/form-data"
+	));
+	curl_setopt($ch, CURLOPT_URL, $url); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields); 
+	$output = curl_exec($ch);	
+}
+
+
+
+
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
 if(!$update)
